4a3616607c2e5a6eb6821b2016ee60da0822e1b7
Terry Petersen <arigorn15@gmail.com>
2024-01-27 17:45:58 -0500

    Setup app routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,56 +1,33 @@
 import React from 'react';
-import { Counter } from '../features/counter/Counter';
 import './App.css';
+import { Root } from '../pages/root/Root';
+import { Cart } from '../pages/cart/Cart';
+import { Checkout } from '../pages/checkout/Checkout';
+import { Home } from '../pages/home/Home';
+import { Login } from '../pages/login/Login';
+import { Order } from '../pages/order/Order';
+import { Orders } from '../pages/orders/Orders';
+import { Product } from '../pages/product/Product';
+import { Products } from '../pages/products/Products';
+import { Registration } from '../pages/registration/Registration';
+import { createRoutesFromElements, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+  const routes = createRoutesFromElements(
+    <Route path='/' element={<Root />}>
+      <Route index element={<Home />} />
+      <Route path='cart' element={<Cart />} />
+      <Route path='checkout' element={<Checkout />} />
+      <Route path='login' element={<Login />} />
+      <Route path='orders' element={<Orders />} />
+      <Route path='orders/:id' element={<Order />} />
+      <Route path='product' element={<Products />} />
+      <Route path='product/:id' element={<Product />} />
+      <Route path='registration' element={<Registration />} />
+    </Route>
   );
+
+  return routes;
 }
 
 export default App;
